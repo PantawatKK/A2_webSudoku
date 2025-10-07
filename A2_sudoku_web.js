1880bbe60cede5bec556b178d779ef1ebccbd74d
PantawatKK <benjawan75680@gmail.com>
2025-10-07 12:02:13 +0700

    A2_sudoku_v3_web.js
    เพิ่ม isConflict

--- A2_sudoku_web.js
+++ A2_sudoku_web.js
@@ -1,14 +1,32 @@
-let grid = Array.from({ length: 9 }, () => Array(9).fill(0));
-let locked = Array.from({ length: 9 }, () => Array(9).fill(false));
+let grid = [
+  [5, 3, 0, 0, 7, 0, 0, 0, 0],
+  [6, 0, 0, 1, 9, 5, 0, 0, 0],
+  [0, 9, 8, 0, 0, 0, 0, 6, 0],
+  [8, 0, 0, 0, 6, 0, 0, 0, 3],
+  [4, 0, 0, 8, 0, 3, 0, 0, 1],
+  [7, 0, 0, 0, 2, 0, 0, 0, 6],
+  [0, 6, 0, 0, 0, 0, 2, 8, 0],
+  [0, 0, 0, 4, 1, 9, 0, 0, 5],
+  [0, 0, 0, 0, 8, 0, 0, 7, 9]
+];
+
 let cell = 60;
 let selected = null;
 let button_y = 560;
+
+let locked = [];
+  for (let r = 0; r < 9; r++) {
+    locked[r] = [];
+    for (let c = 0; c < 9; c++) {
+      locked[r][c] = grid[r][c] !== 0;
 
 function setup() {
   createCanvas(540, 620);
   textAlign(CENTER, CENTER);
   textSize(24);
-  makePuzzle(10);
+
+    }
+  }
 }
 
 function draw() {
@@ -19,12 +37,8 @@ function draw() {
 }
 
 function drawGrid() {
-  for (let i = 0; i <= 9; i++) {
-    if (i % 3 === 0) {
-      strokeWeight(3);
-    } else {
-      strokeWeight(1);
-    }
+  for (let i = 0; i < 10; i++) {
+    strokeWeight(i % 3 === 0 ? 3 : 1);
     line(0, i * cell, 9 * cell, i * cell);
     line(i * cell, 0, i * cell, 9 * cell);
   }
@@ -37,13 +51,18 @@ function drawGrid() {
   }
 }
 
-
 function drawNumbers() {
   textSize(32);
-  fill(0);
   for (let r = 0; r < 9; r++) {
     for (let c = 0; c < 9; c++) {
       if (grid[r][c] !== 0) {
+        if (locked[r][c]) {
+          fill(0);
+        } else if (isConflict(r, c, grid[r][c])) {
+          fill(255, 0, 0);
+        } else {
+          fill(0, 200, 0);
+        }
         text(grid[r][c], c * cell + cell / 2, r * cell + cell / 2);
       }
     }
@@ -74,82 +93,31 @@ function mousePressed() {
     if (i >= 0 && i < 9 && selected) {
       let [r, c] = selected;
       if (!locked[r][c]) {
-        if (isValid(r, c, i + 1)) {
-          grid[r][c] = i + 1;
-        }
+        grid[r][c] = i + 1;
       }
     }
   }
 }
 
-function fillGrid() {
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      if (grid[i][j] === 0) {
-        let nums = shuffle([...Array(9).keys()].map(x => x + 1));
-        for (let num of nums) {
-          if (isValid(i, j, num)) {
-            grid[i][j] = num;
-            if (fillGrid()) return true;
-            grid[i][j] = 0;
-          }
-        }
-        return false;
-      }
-    }
-  }
-  return true;
-}
-
-function makePuzzle(removals = 40) {
-  for (let r = 0; r < 9; r++) {
-    for (let c = 0; c < 9; c++) {
-      grid[r][c] = 0;
-      locked[r][c] = false;
-    }
-  }
-  fillGrid();
-
-  let cells = [];
-  for (let r = 0; r < 9; r++) {
-    for (let c = 0; c < 9; c++) {
-      cells.push([r, c]);
-    }
-  }
-  shuffle(cells, true);
-  let count = 0;
-  for (let [r, c] of cells) {
-    if (count >= removals) break;
-    grid[r][c] = 0;
-    locked[r][c] = false;
-    count++;
-  }
-  for (let r = 0; r < 9; r++) {
-    for (let c = 0; c < 9; c++) {
-      if (grid[r][c] !== 0) locked[r][c] = true;
-    }
-  }
-}
-
-function isValid(row, col, val) {
+function isConflict(row, col, val) {
   for (let c = 0; c < 9; c++) {
-    if (grid[row][c] === val) {
-    return false;
+    if (c !== col && grid[row][c] === val) {
+      return true;
     }
   }
   for (let r = 0; r < 9; r++) {
-    if (grid[r][col] === val) {
-      return false;
+    if (r !== row && grid[r][col] === val) {
+      return true;
     }
   }
   let startRow = row - (row % 3);
   let startCol = col - (col % 3);
   for (let r = startRow; r < startRow + 3; r++) {
     for (let c = startCol; c < startCol + 3; c++) {
-      if (grid[r][c] === val) {
-        return false;
+      if ((r !== row || c !== col) && grid[r][c] === val) {
+        return true;
       }
     }
   }
-  return true;
+  return false;
 }
